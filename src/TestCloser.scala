f519f1cf15eba1bfd87eecce43be45ff356e9075
Yoshimura Yuu <m@yyu.pw>
2017-04-02 02:54:13 +0900

    fix

--- src/TestCloser.scala
+++ src/TestCloser.scala
@@ -1,5 +1,6 @@
 class TestCloser[A] extends Closer[A] {
-  val closeOrder: scala.collection.mutable.ArrayBuffer[A] = scala.collection.mutable.ArrayBuffer.empty[A]
+  val closeOrder: scala.collection.mutable.ArrayBuffer[A] =
+    scala.collection.mutable.ArrayBuffer.empty[A]
 
   def close(r: A): Unit = closeOrder.synchronized {
     closeOrder += r
